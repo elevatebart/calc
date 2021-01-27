05269dbd84c0d246e16ca86c4de43d1c832196db
ElevateBart <ledouxb@gmail.com>
2021-01-27 10:59:24 -0600

    make mount function take snapshots

--- scripts/mount.ts
+++ scripts/mount.ts
@@ -1,16 +1,45 @@
 import $ from "cash-dom";
-import { DefineComponent } from "vue";
 import { MountingOptions } from "@vue/test-utils";
 // @ts-ignore
 import { mount as VTUmount } from "@vue/test-utils/dist/vue-test-utils.esm-bundler";
 
 export default function mount(
-  comp: DefineComponent<{}, {}, any>,
-  options: Omit<MountingOptions<any>, "attachTo">
+  comp: any,
+  options: Omit<MountingOptions<any>, "attachTo"> & { log?: boolean }
 ) {
-  $("body").html("");
-  const $root = $(`<div/>`);
-  $root.html("");
-  $("body").append($root);
-  return VTUmount(comp, { attachTo: $root[0], ...options });
+  const componentName =
+    comp.name ??
+    (() => {
+      const a = comp.__file.split("/");
+      return a[a.length - 1].replace(/.\w+$/, "");
+    })();
+  const message = `<${componentName} ... />`;
+  let logInstance: Cypress.Log;
+
+  return cy.then(() => {
+    if (options.log !== false) {
+      logInstance = Cypress.log({
+        name: "mount",
+        message: [message],
+      });
+    }
+    $("body").html("");
+    const $root = $(`<div/>`);
+    $root.html("");
+    $("body").append($root);
+    const wrapper = VTUmount(comp, { attachTo: $root[0], ...options });
+    return cy
+      .wrap(wrapper, { log: false })
+      .wait(1, { log: false })
+      .then(() => {
+        if (logInstance) {
+          logInstance.snapshot("mounted");
+          logInstance.end();
+        }
+
+        // by returning undefined we keep the previous subject
+        // which is the mounted component
+        return undefined;
+      });
+  });
 }
