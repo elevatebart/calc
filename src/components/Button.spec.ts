a1005c6eb363c3b5b0d967fd0444f67a7789ddc3
ElevateBart <ledouxb@gmail.com>
2021-01-27 10:12:21 -0600

    add button variants

--- src/components/Button.spec.ts
+++ src/components/Button.spec.ts
@@ -5,4 +5,18 @@ describe("<CalcButton />", () => {
   it("should look like a button", () => {
     mount(CalcButton, { slots: { default: "5" } });
   });
+
+  it("should be orange when operator variant", () => {
+    mount(CalcButton, {
+      slots: { default: "+" },
+      props: { variant: "operator" },
+    });
+  });
+
+  it("should be darkened when dark variant", () => {
+    mount(CalcButton, {
+      slots: { default: "AC" },
+      props: { variant: "dark" },
+    });
+  });
 });
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,26 +1,42 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  props: {
+    variant: {
+      type: String,
+      default: null,
+      validator: (val: string) => ["operator", "dark"].includes(val),
+    },
+  },
+});
 </script>
 
 <template>
-  <button @click="$emit('click')">
+  <button @click="$emit('click')" :class="variant">
     <slot />
   </button>
 </template>
 
 <style scoped>
 button {
-  width: 100px;
+  width: 70px;
   height: 50px;
   background: #5d5e5e;
   color: #fff;
-  font-size: 2em;
+  font-size: 1.5em;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  font-weight: 200;
+  font-weight: 300;
   -webkit-appearance: none;
   border: 1px solid #333;
+}
+
+.operator {
+  background: #ff9d00;
+}
+
+.dark {
+  background: #242424;
 }
 </style>
