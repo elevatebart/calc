20caa7071a24b26a57a7780623d3d812e11e6493
ElevateBart <ledouxb@gmail.com>
2021-02-02 15:55:26 -0600

    specs update

--- src/components/Container.spec.tsx
+++ src/components/Container.spec.tsx
@@ -5,10 +5,12 @@ describe("<CalcContainer />", () => {
   it("should look like a mac container", () => {
     mount(() => (
       <CalcContainer>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur cum
-        laborum labore id possimus? Voluptates delectus iure enim nobis maxime
-        illo blanditiis, repellat sunt voluptatibus dignissimos! Assumenda animi
-        non quia.
+        <div style="margin: 10px">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur cum
+          laborum labore id possimus? Voluptates delectus iure enim nobis maxime
+          illo blanditiis, repellat sunt voluptatibus dignissimos! Assumenda animi
+          non quia.
+        </div>
       </CalcContainer>
     ));
   });
